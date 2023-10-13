6aebf4e4c75a0a483272e08951faded43ce6608b
Robert Wolfe <rob@portwallistechnologies.com>
2023-10-13 15:31:06 -0400

    adding details to some of the AS Core Classes

--- src/activitystreams/as_models.py
+++ src/activitystreams/as_models.py
@@ -1,10 +1,47 @@
 from pydantic import BaseModel
 
 class ASObject (BaseModel):
-    pass
-
+    id:str
+    name:str
+    type: str
+    attachment:str
+    attributedTo:str
+    audience:str
+    bcc:str
+    bto:str
+    cc:str
+    content:str
+    context:str
+    duration:str
+    endTime:str
+    generator:str
+    icon:str
+    image:str
+    inReplyTo:str
+    location:str
+    mediaType:str
+    preview:str
+    published:str
+    replies:str
+    startTime:str
+    summary:str
+    tag:str
+    to:str
+    update:str
+    url:str
+    
 class ASLink (BaseModel):
-    pass
+    id:str
+    name:str
+    type:str
+    href:str
+    rel:str
+    mediaType:str
+    hreflang:str
+    height:str
+    width:str
+    preview:str
+    
 
 class ASActivity (ASObject):
     pass 
